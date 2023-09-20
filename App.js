6bc78a5ee9dde3cdc35078be776464d7845ed99b
Dharini S <dharinisrinivasan.94@gmail.com>
2023-09-20 11:46:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
 import React, { Component } from "react";
-import SongCard from "./components/SongCard.js"
-import songs from "./services/song.js"
 
 class App extends Component {
-    state = {
-        songs: songs
-      };
-    
-      sortByTitle = () => {
-        this.setState({
-          songs: [...this.state.songs.sort((a, b) => a.title.toUpperCase() < b.title.toUpperCase() ? -1 : 1)]
-        });
-      };
-    
-      sortByRating = () => {
-        this.setState({
-          songs: [...this.state.songs.sort((a, b) => b.rating - a.rating)]
-        });
-      };
   render() {
     return (
       <div id="super-tunes">
         <h2 id="st-title">SuperTunes - Songs of the Week</h2>
-        <button className="st-btn" onClick={this.sortByTitle}>Sort by Title</button>
-        <button  className="st-btn"onClick={this.sortByRating}>Sort by Rating</button>
-        
-          {this.state.songs.map(song => (
-            
-              <SongCard  data={song} />
-            
-          ))}
-        
       </div>
     );
   }
